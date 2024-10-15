9015630300a415634dab49cb4ad6bac8c9091cbc
blakecraig25 <112400887+blakecraig25@users.noreply.github.com>
2024-10-15 10:42:51 -0400

    Initial commit

---
+++ Assets/Scripts/PickUpScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpScript : MonoBehaviour
+{
+    public GameObject player;
+    public Transform holdPos;
+    
+}
